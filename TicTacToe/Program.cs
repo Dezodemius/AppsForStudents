ea780f245d8bb0e46c0aaedc0eaf044f2185c405
Егор <gladkovyegor@gmail.com>
2021-10-25 20:36:03 +0400

    Init all projects
    - TicTacToe
    - Calculator
    - Tamagotchi
    - ConfigerReader

---
+++ TicTacToe/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace TicTacToe
+{
+  /// <summary>
+  /// Главный класс программы.
+  /// </summary>
+  public class Program
+  {
+    /// <summary>
+    /// Точека входа в программу.
+    /// </summary>
+    /// <param name="args">Аргументы командной строки.</param>
+    public static void Main(string[] args)
+    {
+    }
+  }
+}
