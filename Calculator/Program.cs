67eb46e0e7cfcc71d9b75572abdd2443374b44f1
Егор <gladkovyegor@gmail.com>
2021-10-27 18:40:37 +0400

    Simple calcs

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace Calculator
+﻿using System;
+
+namespace Calculator
 {
   /// <summary>
   /// Главный класс программы.
@@ -11,6 +13,39 @@
     /// <param name="args">Аргументы командной строки.</param>
     public static void Main(string[] args)
     {
+      CalcSimply();
+    }
+
+    private static void CalcSimply()
+    {
+      double result;
+      while (true)
+      {
+        var a = "dawdaw";
+        Console.WriteLine(a[2]);
+        Console.Write("a: ");
+        var leftNumber = double.Parse(Console.ReadLine());
+
+        ConsoleKey action = Console.ReadKey().Key;
+        
+        Console.Write(" b: ");
+        var rightNumber = double.Parse(Console.ReadLine());
+
+        switch (action)
+        {
+         case ConsoleKey.Add:
+           result = leftNumber + rightNumber;
+           break;
+         case ConsoleKey.Subtract:
+           result = leftNumber - rightNumber;
+           break;
+         default:
+           result = -1;
+           break;
+        }
+        Console.WriteLine(result);
+
+      }
     }
   }
 }
